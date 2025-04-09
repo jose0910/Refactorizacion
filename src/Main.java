2f6e87a878843f40b6b230096eaf254c9696ef90
jose0910 <jvazquezalonso@danielcastelao.org>
2025-04-09 14:20:43 +0200

    refactor de  m_score1 a resultadoJ1

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     /**
      * metodo para devolver la puntuacion de tenis
      */
-    public static String getScore(int m_score1, int m_score2) {
+    public static String getScore(int resultadoJ1, int m_score2) {
         String score = "";
         int tempScore=0;
 
-        if (m_score1 == m_score2) {
-            score = ifpuntuacionIgual(m_score1);
+        if (resultadoJ1 == m_score2) {
+            score = ifpuntuacionIgual(resultadoJ1);
         }
-        else if (m_score1 >=4 || m_score2 >=4)
+        else if (resultadoJ1 >=4 || m_score2 >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = resultadoJ1-m_score2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -21,7 +21,7 @@ public class Main {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
+                if (i==1) tempScore = resultadoJ1;
                 else { score+="-"; tempScore = m_score2;}
                 switch(tempScore)
                 {
--- src/readme.md
+++ src/readme.md
@@ -1,1 +1,2 @@
 1. Refactorice el if de las puntuaciones iguales
+2. Refactorice el nombre de m_score1 a resultadoJ1
